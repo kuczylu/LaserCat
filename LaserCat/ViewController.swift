316d140f505e2903e44e146ac42522d95ab8fdf1
Lukas Kuczynski <Lukas_Kuczynski@lukass-mbp-3>
2019-01-03 13:12:05 -0800

    Added basic interface functionality, removed unnecessary scnassets

--- LaserCat/ViewController.swift
+++ LaserCat/ViewController.swift
@@ -10,24 +10,77 @@ import UIKit
 import SceneKit
 import ARKit
 
-class ViewController: UIViewController, ARSCNViewDelegate {
+class ViewController: UIViewController, ARSCNViewDelegate, ARSessionDelegate {
+    
+    var offsetX = 0.0
+    var offsetY = 0.0
+    var offsetZ = 0.0
+    var offsetPan = 0.0
+    var offsetTilt = 0.0
 
     @IBOutlet var sceneView: ARSCNView!
+    @IBOutlet weak var sessionInfoView: UIVisualEffectView!
+    @IBOutlet weak var sessionInfoLabel: UILabel!
+    @IBOutlet weak var valuesView: UIVisualEffectView!
+    @IBOutlet weak var valueLabelX: UILabel!
+    @IBOutlet weak var valueLabelY: UILabel!
+    @IBOutlet weak var valueLabelZ: UILabel!
+    @IBOutlet weak var valueLabelPan: UILabel!
+    @IBOutlet weak var valueLabelTilt: UILabel!
+    
+    
+    @IBAction func toggleValuesButtonClicked(_ sender: UIButton) {
+        let isHidden = !valuesView.isHidden
+        valuesView.isHidden = isHidden
+        
+        if isHidden {
+            sender.setTitle("show values", for: .normal)
+        } else {
+            sender.setTitle("hide values", for: .normal)
+        }
+    }
+    
+    
+    @IBAction func resetButtonClicked() {
+        resetTracking()
+    }
+    
+    @IBAction func stepperValueChangedX(_ sender: UIStepper) {
+        offsetX = sender.value
+        valueLabelX.text = String(format: "%.1f", offsetX)
+    }
+    
+    @IBAction func stepperValueChangedY(_ sender: UIStepper) {
+        offsetY = sender.value
+        valueLabelY.text = String(format: "%.1f", offsetY)
+    }
+    
+    @IBAction func stepperValueChangedZ(_ sender: UIStepper) {
+        offsetZ = sender.value
+        valueLabelZ.text = String(format: "%.1f", offsetZ)
+    }
+    
+    @IBAction func stepperValueChangedPan(_ sender: UIStepper) {
+        offsetPan = sender.value
+        valueLabelPan.text = String(format: "%.1f", offsetPan)
+    }
+    
+    @IBAction func stepperValueChangedTilt(_ sender: UIStepper) {
+        offsetTilt = sender.value
+        valueLabelTilt.text = String(format: "%.1f", offsetTilt)
+    }
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        guard ARWorldTrackingConfiguration.isSupported else {
+            fatalError("ARKit is not supported on this device")
+        }
+        
         // Set the view's delegate
         sceneView.delegate = self
-        
-        // Show statistics such as fps and timing information
-        sceneView.showsStatistics = true
-        
-        // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
-        
-        // Set the scene to the view
-        sceneView.scene = scene
+        sceneView.debugOptions = [.showFeaturePoints]
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -38,6 +91,8 @@ class ViewController: UIViewController, ARSCNViewDelegate {
 
         // Run the view's session
         sceneView.session.run(configuration)
+        sceneView.session.delegate = self
+        UIApplication.shared.isIdleTimerDisabled = true
     }
     
     override func viewWillDisappear(_ animated: Bool) {
@@ -47,29 +102,74 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         sceneView.session.pause()
     }
 
-    // MARK: - ARSCNViewDelegate
+    // MARK: - ARSessionDelegate
     
-/*
-    // Override to create and configure nodes for anchors added to the view's session.
-    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
-        let node = SCNNode()
-     
-        return node
+    func session(_ session: ARSession, didAdd anchors: [ARAnchor]) {
+        guard let frame = session.currentFrame else { return }
+        updateSessionInfoLabel(for: frame, trackingState: frame.camera.trackingState)
     }
-*/
+    
+    
+    func session(_ session: ARSession, didRemove anchors: [ARAnchor]) {
+        guard let frame = session.currentFrame else { return }
+        updateSessionInfoLabel(for: frame, trackingState: frame.camera.trackingState)
+    }
+    
+    
+    func session(_ session: ARSession, cameraDidChangeTrackingState camera: ARCamera) {
+        updateSessionInfoLabel(for: session.currentFrame!, trackingState: camera.trackingState)
+    }
+    
+    
+    func session(_ session: ARSession, didUpdate frame: ARFrame) {
+        
+    }
+    
+    
+    // MARK: ARSessionObserver
     
     func session(_ session: ARSession, didFailWithError error: Error) {
-        // Present an error message to the user
-        
+        sessionInfoLabel.text = "Session failed: \(error.localizedDescription)"
+        resetTracking()
     }
+    
     
     func sessionWasInterrupted(_ session: ARSession) {
-        // Inform the user that the session has been interrupted, for example, by presenting an overlay
-        
+        sessionInfoLabel.text = "Session was interrupted"
     }
     
+    
     func sessionInterruptionEnded(_ session: ARSession) {
-        // Reset tracking and/or remove existing anchors if consistent tracking is required
+        sessionInfoLabel.text = "Session interruption ended"
+        resetTracking()
+    }
+    
+    
+    private func updateSessionInfoLabel(for frame: ARFrame, trackingState: ARCamera.TrackingState) {
         
+        let message: String
+        
+        switch trackingState {
+        case .normal where frame.anchors.isEmpty:
+            message = "Tap anywhere to lasercat!"
+        case .notAvailable:
+            message = "Tracking unavailable."
+        case .limited(.excessiveMotion):
+            message = "Tracking limited - Move device more slowly."
+        case .limited(.insufficientFeatures):
+            message = "Tracking limited - Point the device at an area with visible surface detail, or improve lighting conditions."
+        case .limited(.initializing):
+            message = "Initializing AR session."
+        default:
+            message = ""
+        }
+        
+        sessionInfoLabel.text = message
+        sessionInfoView.isHidden = message.isEmpty
+    }
+
+    private func resetTracking() {
+        let configuration = ARWorldTrackingConfiguration()
+        sceneView.session.run(configuration, options: [.resetTracking, .removeExistingAnchors])
     }
 }
