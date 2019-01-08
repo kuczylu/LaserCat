01549ec1fbb2472d0c4eb9a22d7b338f42b75402
Lukas Kuczynski <Lukas_Kuczynski@lukass-macbook-pro-3.local>
2019-01-08 13:29:14 -0800

    Added fade animation and self destruct to laser node, changed laser color to green

--- LaserCat/CatNode.swift
+++ LaserCat/CatNode.swift
@@ -9,8 +9,8 @@
 import Foundation
 import SceneKit
 
-
-class CatNode: SCNNode {
+// Watchcout! setting delegate to self then deleting node w/ animation may have retain cycle issues
+class CatNode: SCNNode, CAAnimationDelegate {
     
     init(_ catTransform: SCNMatrix4, _ laserLength: Float) {
         super.init()
@@ -39,12 +39,33 @@ class CatNode: SCNNode {
     private func getLaserNode(_ length: Float) -> SCNNode {
         let cylinder = SCNCylinder(radius: 0.01, height: CGFloat(length))
         cylinder.radialSegmentCount = 8
+        cylinder.firstMaterial?.diffuse.contents = UIColor.green
         let node = SCNNode(geometry: cylinder)
         node.simdPosition = simd_float3(0.0, 0.0, 0.5 * length)
         node.simdRotation = simd_float4(1.0, 0.0, 0.0, .pi / 2.0)
-
         node.name = "laser"
+        
+        let animation = CABasicAnimation(keyPath: "opacity")
+        animation.fromValue = 1.0
+        animation.toValue = 0.0
+        animation.duration = 3.0 //seconds
+        animation.fillMode = .forwards
+        animation.isRemovedOnCompletion = false
+        animation.delegate = self
+        node.addAnimation(animation, forKey: "opacityFade")
         
         return node
     }
+    
+    // MARK: - CAAnimationDelegate
+    
+    func animationDidStop(_ anim: CAAnimation, finished flag: Bool) {
+        // ??? check here if correct animation
+        anim.delegate = nil // fixes retain issue??
+        while let node = self.childNode(withName: "laser", recursively: false) {
+            node.removeFromParentNode()
+        }
+    }
 }
+
+
